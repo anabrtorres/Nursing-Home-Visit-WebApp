d63ca1d725744febb1ff6c39dfd2346f84126999
Ana Torres <ana.beatriz_19@hotmail.com>
2020-10-21 13:15:17 -0300

    update 1.0

--- src/database/test.js
+++ src/database/test.js
@@ -3,9 +3,25 @@ const saveNursingHome = require("./saveNursingHome");
 
 Database.then(async (db) => {
   // inserir dados na tabela
-  /*await saveNursingHome(db, {
-    lat: "-20.1413335",
-    lng: "-44.8761516",
+  // await saveNursingHome(db, {
+  //   lat: "-20.1413335",
+  //   lng: "-44.8761516",
+  //   name: "Vila Vicentina",
+  //   about:"Presta assistência a idosos tanto dependentes quanto independentes, que precisam de assistência e cuidados especializados.",
+  //   whatsapp: "999999999",
+  //   images: [
+  //     "https://media.npr.org/assets/img/2020/03/04/coronanursing-1_wide-1f2966692dda24ecb0e603b5abc1c3fa7d4734ec.jpg?s=1400",
+      
+  //     "https://ei.marketwatch.com/Multimedia/2019/12/19/Photos/ZQ/MW-HX239_nursin_20191219165234_ZQ.jpg?uuid=dd09742e-22a9-11ea-b4e5-9c8e992d421e",
+  //   ].toString(),
+  //   instructions: "Vista um sorriso no rosto e traga muito amor e paciência.",
+  //   opening_hours: "Horário de visitas das 8h às 18h",
+  //   open_on_weekends: "1"
+  // })
+
+  await saveNursingHome(db, {
+    lat: "-20.1382206",
+    lng: "-44.8926312",
     name: "Vila Vicentina",
     about:"Presta assistência a idosos tanto dependentes quanto independentes, que precisam de assistência e cuidados especializados.",
     whatsapp: "999999999",
@@ -16,8 +32,8 @@ Database.then(async (db) => {
     ].toString(),
     instructions: "Vista um sorriso no rosto e traga muito amor e paciência.",
     opening_hours: "Horário de visitas das 8h às 18h",
-    open_on_weekends: "1"
-  })*/
+    open_on_weekends: "0"
+  })
 
   // consultar dados da tabela
   const selectedNursingHome = await db.all("SELECT * FROM nursingHomes");
